dfd1b8904bc264a968691f8cbceb5d5a3acbdb0a
Linde Guilhem <guilhem.linde@epitech.eu>
2022-12-17 16:59:58 +0100

    download characters data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import { GetStaticProps, NextPage } from "next";
-import { Character, GetCharacterResults } from "../types";
-import imageLoader from "../utils/imageLoader";
-import Link from "next/link";
+import { NextPage } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const Home: NextPage = ({ characters }: any) => {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -18,44 +14,9 @@ const Home: NextPage = ({ characters }: any) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {characters.map((character: Character) => {
-        return (
-          <div key={character.id}>
-            <Link href={`/characters/${character.id}`}>
-              <h3>{character.name}</h3>
-
-              <Image
-                loader={imageLoader}
-                unoptimized
-                src={`/jpeg-characters/${character.id}.jpeg`}
-                alt={character.name}
-                width="200"
-                height="200"
-              />
-            </Link>
-          </div>
-        );
-      })}
       <main className={styles.main}></main>
     </>
   );
 };
 
 export default Home;
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  let characters = [];
-  for (let i = 1; i < 42; i++) {
-    const res = await fetch(
-      `https://rickandmortyapi.com/api/character?page=${i}`
-    );
-    const { results } = await res.json();
-    characters.push(...results);
-  }
-
-  return {
-    props: {
-      characters,
-    },
-  };
-};
