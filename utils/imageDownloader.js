af28f5f01e06fb267b2337f6c31a60ad311eea0a
Linde Guilhem <guilhem.linde@epitech.eu>
2022-12-17 14:29:45 +0100

    build ok

--- utils/imageDownloader.js
+++ utils/imageDownloader.js
@@ -1,29 +1,29 @@
-const download = require("image-downloader");
+// const download = require("image-downloader");
 
-function downloadImage(url, filepath) {
-  console.log(url, filepath);
-  return download.image({
-    url,
-    dest: filepath,
-  });
-}
+// function downloadImage(url, filepath) {
+//   console.log(url, filepath);
+//   return download.image({
+//     url,
+//     dest: filepath,
+//   });
+// }
 
-const forLoop = async (_) => {
-  console.log("Start");
+// const forLoop = async (_) => {
+//   console.log("Start");
 
-  for (let index = 1; index <= 826; index++) {
-    const image = await downloadImage(
-      `https://rickandmortyapi.com/api/character/avatar/${index}.jpeg`,
-      "../../public"
-    );
-    console.log(index, image);
-  }
+//   for (let index = 1; index <= 826; index++) {
+//     const image = await downloadImage(
+//       `https://rickandmortyapi.com/api/character/avatar/${index}.jpeg`,
+//       "../../public"
+//     );
+//     console.log(index, image);
+//   }
 
-  console.log("End");
-};
+//   console.log("End");
+// };
 
-try {
-  forLoop();
-} catch (e) {
-  console.log(e);
-}
+// try {
+//   forLoop();
+// } catch (e) {
+//   console.log(e);
+// }
